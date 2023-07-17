b310bf1266f39e0dfaec1d2adc9df169f738e25f
Aayush Vishwakarma <72176709+bugholic@users.noreply.github.com>
2023-07-17 12:44:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ let showMainArea = document.getElementById('showMainArea')
 let home = document.getElementById('home') 
 let skills = document.getElementById('skills') 
 let projects = document.getElementById('projects') 
+let projectCategory = document.getElementById('projectCategory') 
 
 window.addEventListener('load',()=>{
     showMainArea.removeChild(home)
     showMainArea.removeChild(skills)
     showMainArea.removeChild(projects)
+    showMainArea.removeChild(projectCategory)
     
 })
 
@@ -15,6 +17,7 @@ function toggle(el){
     home.classList.add('hidden')
     skills.classList.add('hidden')
     projects.classList.add('hidden')
+    projectCategory.classList.add('hidden')
     
 
     showMainArea.appendChild(el)    
